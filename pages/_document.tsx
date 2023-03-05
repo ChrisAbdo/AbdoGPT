932e7513ddbebf8660848181b86c9ea0644dd191
Chris Abdo <chrisabdo@Chriss-Mac-mini.fios-router.home>
2023-03-05 14:45:07 -0500

    ADDED: redeploy for env

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,22 +6,13 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <link rel="icon" href="/favicon.ico" />
-          <meta
-            name="description"
-            content="Generate your next Twitter bio in seconds."
-          />
+          <meta name="description" content="AbdoGPT" />
           <meta property="og:site_name" content="twitterbio.com" />
-          <meta
-            property="og:description"
-            content="Generate your next Twitter bio in seconds."
-          />
+          <meta property="og:description" content="AbdoGPT" />
           <meta property="og:title" content="Twitter Bio Generator" />
           <meta name="twitter:card" content="summary_large_image" />
           <meta name="twitter:title" content="Twitter Bio Generator" />
-          <meta
-            name="twitter:description"
-            content="Generate your next Twitter bio in seconds."
-          />
+          <meta name="twitter:description" content="AbdoGPT" />
           <meta
             property="og:image"
             content="https://twitterbio.com/og-image.png"
